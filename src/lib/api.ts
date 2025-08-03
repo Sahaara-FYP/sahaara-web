e3828d0f2d1368583e3439c773a5d6461283d81f
Ayyan Ali <rayyanali2705@gmail.com>
2025-08-03 21:10:12 +0500

    minor chagnes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@ import { toast } from "sonner";
 import { manualLogout } from "@/contexts/AuthContext";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api", //Backend API url
+  baseURL: import.meta.env.VITE_API_URL || "http://192.168.0.36:8000/api", //Backend API url
   withCredentials: true,
 });
 
